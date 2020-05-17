48cdbedde363a7513164afa782646e5967ba4ed7
maxim.vinogradov <maxim.vinogradov@fastdev.se>
2020-05-17 23:48:05 +0400

    added bluetooth info

--- app/src/main/java/com/example/httpclien/MainActivity.kt
+++ app/src/main/java/com/example/httpclien/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.httpclien
 
+import android.bluetooth.BluetoothAdapter
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
 import java.net.HttpURLConnection
 import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bluetooth = BluetoothAdapter.getDefaultAdapter()
+        val status: String
+        status = if(bluetooth != null) {
+            if (bluetooth.isEnabled()) {
+                var myDeviceAddress: String = bluetooth.getAddress()
+                var myDeviceName: String = bluetooth.getName()
+                var myDeviceStatus: Int = bluetooth.getState()
+                "$myDeviceAddress : $myDeviceName : status = $myDeviceStatus"
+            } else {
+                "Bluetooth отключен"
+            }
+        } else {
+            "Bluetooth null"
+        }
+        val toast = Toast.makeText(applicationContext, status, Toast.LENGTH_LONG)
+        toast.show()
     }
 
     inner class AsyncTaskHandleJson : AsyncTask<String, String, String>() {
